191f876345d3765eec73b6a0e7efd51b06a7fcdb
Julian Rudolph <julian.rudolphsc@gmail.com>
2015-11-21 11:51:12 -0500

    Added basic structure to js

---
+++ js/control.js
@@ -0,0 +1,23 @@
+var pictures = [
+
+	{
+	"title":"Elephant Ass Thailand",
+	"description":"A Thai elephants ass, in all it's glory",
+	"url":"../img/eleass.jpg"
+	},
+	{
+	"title":"Historic House Thailand",
+	"description":"A historic house in Thailand",
+	"url":"../img/Historic-House-Thailand.jpg"
+	},
+	{
+	"title":"Thailand Beach",
+	"description":"Thai Beach",
+	"url":"../img/Thailand-2.jpg:
+	},
+	{
+	"title":"Bangkok House",
+	"description":"Beautiful house in BKK",
+	"url":"../img/Thailandbkk.jpg"
+	}
+];
